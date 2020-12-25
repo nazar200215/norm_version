bd28e64c0e7ddab1ba7860ead4321c941ea0203e
nazar200215 <nazar.tovstenko02@gmail.com>
2020-12-26 00:04:58 +0200

    n

---
+++ char_of_roads.py
@@ -0,0 +1,44 @@
+LINE_NUMBERS = {
+    "DirtRoad": 1,
+    "SimpleCityRoad": 2,
+    "CityExpressRoad": 4,
+    "Highway": 2,
+    "Freeway": 4
+}
+
+ROAD_SURFACE = (
+    "Asphalt", "Composite pavement", "Concrete"
+)
+
+# LINE_WIDTH = {
+#     "DirtRoad": 4.5,
+#     "SimpleCityRoad": 3.5,
+#     "CityExpressRoad": 3.75,
+#     "Highway": 3.75,
+#     "Freeway": 3.75
+# }
+
+SEPERATE_LINE_WIDTH = 0.2
+
+CENTRAL_SL_WIDTH = {
+    "SimpleCityRoad": 0.5,
+    "CityExpressRoad": 4,
+    "Highway": 0.5,
+    "Freeway": 6
+}
+
+MAX_TRAFFIC = {
+    "DirtRoad": 2,
+    "SimpleCityRoad": 4,
+    "CityExpressRoad": 6,
+    "Highway": 10,
+    "Freeway": 14
+}
+
+MAX_SPEED = {
+    "DirtRoad": 40,
+    "SimpleCityRoad": 60,
+    "CityExpressRoad": 90,
+    "Highway": 110,
+    "Freeway": 160
+}
---
+++ roads.py
@@ -0,0 +1,63 @@
+import sys
+
+sys.path.append("ka-8226\\src\\constants\\roads_constants.py")
+from char_of_roads import *
+
+
+class Road():
+    _name = None
+
+    def __init__(self):
+        self._line_numbers = LINE_NUMBERS[self._name]
+        # self._line_width = LINE_WIDTH[self._name]
+        self._bus_line = False
+        # self._roadway_width = _line_numbers
+        self._max_traffic = MAX_TRAFFIC[self._name]
+        self._fence = False
+        self._max_speed = MAX_SPEED[self._name]
+
+    # возможно переделаю на осевую нагрузку
+    def max_weight(self):
+        return 0
+
+    def get_max_traffic(self):
+        return self._max_traffic
+
+    def get_max_speed(self):
+        return self._max_speed
+
+
+class DirtRoad(Road):
+    def __init__(self):
+        self._name = "DirtRoad"
+        super().__init__()
+
+    def get_stack(self, transport):
+        return transport.get_weight > self.max_weight()
+
+
+class SimpleCityRoad(Road):
+    def __init__(self):
+        self._name = "SimpleCityRoad"
+        super().__init__()
+        self._central_separate_line = True
+        self._bus_line = True
+
+
+class CityExpressRoad(Road):
+    def __init__(self):
+        self._name = "CityExpressRoad"
+        # Две 4 полосы
+        super().__init__()
+
+
+class Highway(Road):
+    def __init__(self):
+        self._name = "Highway"
+        super().__init__()
+
+
+class Freeway(Road):
+    def __init__(self):
+        self._name = "Freeway"
+        super().__init__()
